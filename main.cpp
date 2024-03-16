e56bf45464c262299ccb574e878c9dae9ed77436
deccer <decccer@outlook.com>
2024-03-16 17:53:36 +0100

    With glad

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
-#include <GL/gl.h>
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
 int main() {
-    gladLoadGL();
+    if (glfwInit() == GLFW_FALSE)
+    {
+        return 0;
+    }
+
+    if (gladLoadGL(glfwGetProcAddress) == 0)
+    {
+        return 0;
+    }
     return 0;
 }
